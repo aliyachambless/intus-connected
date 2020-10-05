264a81293d7823a871e65f8278a60587151fcd2a
vxu4 <vixixu68@gmail.com>
2020-10-05 10:57:07 -0400

    resolving errors

--- connected-website/src/App.jsx
+++ connected-website/src/App.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Connected from './components/Connected';
 
 function App() {
   return (
     <div className="App w-100 h-100 d-flex flex-column">
-      <Switch>
-        <Route exact path="/404" component={NotFound} />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/Art" component={Art} />
-        <Route component={RedirectToNotFound} />
-      </Switch>
+      <Connected />
     </div>
   );
 }
--- connected-website/src/components/Connected.jsx
+++ connected-website/src/components/Connected.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import ScrollableAnchor from 'react-scrollable-anchor';
 import { configureAnchors } from 'react-scrollable-anchor'
-import { Link, withRouter } from 'react-router-dom';
 import connected from '../assets/connected-logos-indigo.png';
+import '../styles/Utilities.scss';
 
 configureAnchors({ scrollDuration: 700 });
 
 const Connected = () => {
   return (
-    <ScrollableAnchor id={'film'}>
-    <div className="h-100 w-100 bg-white d-flex flex-row justify-content-center align-items-center">
-      <img src={connected} alt="Connected logo" />
+    <div className="h-100 w-100 bg-white d-flex flex-row justify-content-center align-items-center" >
+      <img src={connected} alt="Connected logo" className="img70"/>
     </div>
-    </ScrollableAnchor>
   )
 }
 
