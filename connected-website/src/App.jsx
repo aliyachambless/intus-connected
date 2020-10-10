7dc5ba22012659b7aa4aa496e162e94139f49e5e
Aliya Chambless <aliyachambless@Aliyas-MacBook-Pro.local>
2020-10-10 10:39:34 -0400

    about section

--- connected-website/src/App.jsx
+++ connected-website/src/App.jsx
@@ -3,11 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Connected from './components/Connected';
 import Impact from './components/Impact';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App h-100 d-flex flex-column">
       <Connected />
+      <About />
       <Impact />
     </div>
   );
