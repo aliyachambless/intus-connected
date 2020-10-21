6d5e4f3309ed242d76d9c74edca330e8710010a6
vxu4 <vixixu68@gmail.com>
2020-10-21 18:20:56 -0400

    adding involved button

--- connected-website/src/components/Involved.jsx
+++ connected-website/src/components/Involved.jsx
@@ -15,24 +15,39 @@ configureAnchors({ scrollDuration: 700 });
 const Involved = () => {
   return (
     <div className="involved-background">
-    <Container
+      <Container
         fluid
-        className="w-100 p-0 px-5"
-    >
-        <Row className="d-flex justify-content-center align-items-end text-white font-weight-bold p-5 m-5">
-        <h1 className="p-0 m-0 extra-bold">get involved;</h1>
+        className="w-100 p-0 px-5 bg-black"
+      >
+        <ScrollableAnchor id={'involved'}>
+          <Row className="d-flex justify-content-center align-items-end text-white font-weight-bold pt-5 m-5">
+            <h1 className="p-0 m-0 extra-bold">get involved</h1>
+          </Row>
+        </ScrollableAnchor>
+        <Row className="d-flex flex-row justify-content-center align-items-center pb-5 mb-5 ">
+          <div className="position-relative">
+          <a
+            className="form-link "
+            href="https://forms.gle/ykD6ZPyRhynVq6LfA"
+            target="_blank"
+          >
+            <div className="px-5 py-3 rounded-lg border-white border">
+              <h4 className="font-weight-bold text-white text-middle p-0 m-0">Volunteer</h4>
+            </div>
+          </a>
+          </div>
         </Row>
-        <Row className="px-5 mx-5 pt-5 d-flex flex-column justify-content-left align-items-start text-white font-weight-bold">
-        <h2 className="font-weight-bold text-white-60">Application Process</h2>
-        <p className="text-white px-4 py-5 mx-5">
+        {/* <Row className="px-5 mx-5 pt-5 d-flex flex-column justify-content-left align-items-start text-white font-weight-bold">
+          <h2 className="font-weight-bold text-white-60">Application Process</h2>
+          <p className="text-white px-4 py-5 mx-5">
             connected by Intus Care is an intergenerational connection program fostering relationships between our youth and elderly
             connected by Intus Care is an intergenerational connection program fostering relationships between our youth and elderly
             connected by Intus Care is an intergenerational connection program fostering relationships between our youth and elderly
             connected by Intus Care is an intergenerational connection program fostering relationships between our youth and elderly
             connected by Intus Care is an intergenerational connection program fostering relationships between our youth and elderly
         </p>
-        </Row>
-    </Container>
+        </Row> */}
+      </Container>
     </div>
   )
 }
